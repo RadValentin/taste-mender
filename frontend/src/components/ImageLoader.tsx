7afab04f8e2143a39c7e36ab6fbdbc7f6fe85297
Valentin Radulescu <radulescu.valentin@gmail.com>
2026-08-05 14:56:55 +0300

    Fade in images when they're loaded

--- frontend/src/components/ImageLoader.tsx
+++ frontend/src/components/ImageLoader.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./ImageLoader.css";
 
 type ImageLoaderProps = {
   src: string | null,
@@ -10,15 +11,24 @@ type ImageLoaderProps = {
  *  when the image is null or fails to load. Used for album cover art. */
 export default function ImageLoader({ src, alt, fallback }: ImageLoaderProps) {
   const [imgError, setImgError] = useState(false);
+  const [imgLoaded, setImgLoaded] = useState(false);
 
   if (src && !imgError) {
     return (
-      <img src={src} alt={alt} loading="lazy" onError={() => setImgError(true)} />
+      <img
+        className="image-loader"
+        src={src}
+        alt={alt}
+        loading="lazy"
+        onError={() => setImgError(true)}
+        onLoad={() => setImgLoaded(true)}
+        style={{opacity: imgLoaded ? 1 : 0}}
+      />
     );
   } else {
     return (
-      <div className="image-fallback">
-        { fallback || "?" }
+      <div className="image-loader image-loader--fallback">
+        {fallback || "?"}
       </div>
     );
   }
