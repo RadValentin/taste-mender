9998024ac21dae5928c9ea909636ce7412145349
Valentin Radulescu <radulescu.valentin@gmail.com>
2026-09-21 14:38:49 +0300

    Switch react linter and fix eslint config errors

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,22 +1,22 @@
-import js from '@eslint/js'
+import eslintReact from '@eslint-react/eslint-plugin'
+import eslintJs from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default tseslint.config([
+export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
-      js.configs.recommended,
+      eslintJs.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      eslintReact.configs["recommended-typescript"],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
       globals: globals.browser,
     },
   },
